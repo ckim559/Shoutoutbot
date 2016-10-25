766747c2beddb11804ad0ec574a4cda9e77f4574
ckim559 <ckim@freewheel.tv>
2016-10-25 16:18:19 -0400

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -40,6 +40,9 @@ let createCase = newCase => {
 		c.set('Resource_Assignment_Due_Date__c', newCase.date)
 		c.set('Project_Scope__c', newCase.scope)
 		c.set('Submitted_By__c', newCase.name)
+		c.set('Project_Target_Start_Date__c', newCase.start)
+		c.set('Project_Target_Live_Date__c', newCase.live)
+		c.set('Estimated_Hours__c', newCase.hours)
 		c.set('Submitter_Email__c', newCase.email)
 		c.set('Account_Manager__c', newCase.AM)
 		c.set('Project_Manager__c', newCase.PJM)
@@ -63,7 +66,7 @@ let createCase = newCase => {
 let findmyopencases = email => {
 
     return new Promise((resolve, reject) => {
-         let q = "SELECT Id, CaseNumber, Subject, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AM_Req__c, AS_Req__c, PJM_Req__c, SE_Req__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status <> 'Closed' ORDER BY Resource_Assignment_Due_Date__c ASC LIMIT 20";
+         let q = "SELECT Id, CaseNumber, Subject, Estimated_Hours__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AM_Req__c, AS_Req__c, PJM_Req__c, SE_Req__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status <> 'Closed' ORDER BY Resource_Assignment_Due_Date__c ASC LIMIT 20";
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred");
@@ -78,10 +81,25 @@ let findmyopencases = email => {
 let findmyclosedcases = email => {
 
     return new Promise((resolve, reject) => {
-         let q = "SELECT Id, Subject, CaseNumber, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status = 'Closed' ORDER BY Resource_Assignment_Due_Date__c DESC LIMIT 20";
+         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status = 'Closed' ORDER BY Resource_Assignment_Due_Date__c DESC LIMIT 20";
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred");
+            } else {
+                resolve(resp.records);
+            }
+        });
+    });
+
+};
+
+let findcasenumber = casenumber => {
+
+    return new Promise((resolve, reject) => {
+         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Project_Target_Start_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE CaseNumber = '" + casenumber + "' AND RecordTypeId = '012600000005OU0' LIMIT 20";
+        org.query({query: q}, (err, resp) => {
+            if (err) {
+                reject("An error as occurred. Please make sure to format the request as follows: Find Case Number 1234");
             } else {
                 resolve(resp.records);
             }
@@ -96,4 +114,5 @@ login();
 exports.org = org;
 exports.findmyopencases = findmyopencases;
 exports.findmyclosedcases = findmyclosedcases;
+exports.findcasenumber = findcasenumber;
 exports.createCase = createCase;
