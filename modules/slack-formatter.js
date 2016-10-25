1a7b6bdc778a5bf253773faa724740ec16711412
ckim559 <ckim@freewheel.tv>
2016-10-25 16:18:33 -0400

    Update slack-formatter.js

--- modules/slack-formatter.js
+++ modules/slack-formatter.js
@@ -22,8 +22,11 @@ let formatOpenCases = cases => {
 			fields.push({title: "Case Number:", value: _case.get("CaseNumber"), short: true});
 			fields.push({title: "Case Status:", value: _case.get("Status"), short: true});
 			fields.push({title: "Assignment Due Date:", value: _case.get("Resource_Assignment_Due_Date__c"), short: true});
+			fields.push({title: "Project Target Start Date:", value: _case.get("Project_Target_Start_Date__c"), short: true});
+			fields.push({title: "Project Target Life Date:", value: _case.get("Project_Target_Live_Date__c"), short: true});
 			fields.push({title: "Project Description:", value: _case.get("Description"), short: false});
 			fields.push({title: "Project Scope:", value: _case.get("Project_Scope__c"), short: false});
+			fields.push({title: "Estimated Hours:", value: _case.get("Estimated_Hours__c,"), short: false});
 			fields.push({title: "Project Manager Required?", value: _case.get("PJM_Req__c"), short: true});
 			fields.push({title: "Account Manager Required?:", value: _case.get("AM_Req__c"), short: true});
 			fields.push({title: "Solutions Engineer Required?:", value: _case.get("SE_Req__c"), short: true});
@@ -49,8 +52,11 @@ let formatClosedCases = cases => {
 			fields.push({title: "Case Number:", value: _case.get("CaseNumber"), short: true});
 			fields.push({title: "Case Status:", value: _case.get("Status"), short: true});
 			fields.push({title: "Assignment Due Date:", value: _case.get("Resource_Assignment_Due_Date__c"), short: true});
+			fields.push({title: "Project Target Start Date:", value: _case.get("Project_Target_Start_Date__c"), short: true});
+			fields.push({title: "Project Target Life Date:", value: _case.get("Project_Target_Live_Date__c"), short: true});
 			fields.push({title: "Project Description:", value: _case.get("Description"), short: false});
 			fields.push({title: "Project Scope:", value: _case.get("Project_Scope__c"), short: false});
+			fields.push({title: "Estimated Hours:", value: _case.get("Estimated_Hours__c,"), short: false});
 			fields.push({title: "Project Manager:", value: _case.get("Project_Manager_Assigned__c"), short: true});
 			fields.push({title: "Account Manager:", value: _case.get("Account_Manager_Assigned__c"), short: true});
 			fields.push({title: "Solutions Engineer(s):", value: _case.get("SE_assigned_to_GTM_Project__c"), short: true});
