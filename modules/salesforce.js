245534a412afd49e81a8ad53a34aa085d7c24558
ckim559 <ckim@freewheel.tv>
2017-03-21 13:05:59 -0400

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -69,7 +69,76 @@ let findmyopencases = email => {
          let q = "SELECT Id, CaseNumber, Subject, Estimated_Hours__c, Submitted_By__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AM_Req__c, AS_Req__c, PJM_Req__c, SE_Req__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status <> 'Closed' ORDER BY Resource_Assignment_Due_Date__c ASC LIMIT 20";
         org.query({query: q}, (err, resp) => {
             if (err) {
-                reject("An error as occurred");
+ org = nforce.createConnection({
+		environment: 'sandbox',
+        clientId: SF_CLIENT_ID,
+        clientSecret: SF_CLIENT_SECRET,
+        redirectUri: 'http://localhost:3000/oauth/_callback',
+        mode: 'single',
+        autoRefresh: true
+    });
+
+let login = () => {
+
+    org.authenticate({username: SF_USER_NAME, password: SF_PASSWORD}, err => {
+        if (err) {
+            console.error("Authentication error");
+            console.error(err);
+        } else {
+            console.log("Authentication successful");
+        }
+    });
+
+};
+
+let createShoutout = newShoutout => {
+
+    return new Promise((resolve, reject) => {
+		
+        let c = nforce.createSObject('Services_Shoutout__c');
+        c.set('Recipient_Name__c', newShoutout.person);
+		c.set('Reason__c', newShoutout.reason);
+		c.set('Shoutout_Week__c', newShoutout.id);
+		
+        org.insert({sobject: c}, err => {
+            if (err) {
+                console.error(err);
+                reject("An error occurred while creating a Shoutout");
+            } else {
+                resolve(c);
+            }
+        });
+    });
+
+};
+
+let createVote = newVote => {
+
+    return new Promise((resolve, reject) => {
+		
+        let c = nforce.createSObject('Shoutout_Vote__c');
+		c.set('Services_Shoutout__c', newVote.id);
+		
+        org.insert({sobject: c}, err => {
+            if (err) {
+                console.error(err);
+                reject("An error occurred while casting your vote.");
+            } else {
+                resolve(c);
+            }
+        });
+    });
+
+};
+
+
+let findshoutouts = shouts => {
+
+    return new Promise((resolve, reject) => {
+         let q = "SELECT Reason__c, Request_Number__c, Shoutout__c, Recipient_Name__c FROM Services_Shoutout__c WHERE Shoutout_Week__r.Open_Voting__c = TRUE ORDER BY Name ASC";
+        org.query({query: q}, (err, resp) => {
+            if (err) {
+                reject("An error as occurred. Fix yo code!");
             } else {
                 resolve(resp.records);
             }
@@ -78,59 +147,15 @@ let findmyopencases = email => {
 
 };
 
-let findmyclosedcases = email => {
+let findshoutoutweek = week => {
 
     return new Promise((resolve, reject) => {
-         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Submitted_By__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE Submitter_Email__c LIKE '%" + email + "%' AND RecordTypeId = '012600000005OU0' AND Status = 'Closed' ORDER BY Resource_Assignment_Due_Date__c DESC LIMIT 20";
-        org.query({query: q}, (err, resp) => {
+         let q = "SELECT ID, Name FROM Shoutout_Week__c WHERE Open_Voting__c = TRUE LIMIT 1";
+         org.query({query: q}, (err, resp) => {
             if (err) {
-                reject("An error as occurred");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findcasenumber = casenumber => {
-
-    return new Promise((resolve, reject) => {
-         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Submitted_By__c, Project_Target_Start_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE CaseNumber = '" + casenumber + "' AND RecordTypeId = '012600000005OU0' LIMIT 20";
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred. Please make sure to format the request as follows: Find Case Number 1234");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findcaseowner = criteria => {
-
-    return new Promise((resolve, reject) => {
-         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Submitted_By__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE Submitted_By__c LIKE '%" + criteria + "%' AND RecordTypeId = '012600000005OU0' ORDER BY Resource_Assignment_Due_Date__c DESC LIMIT 20";
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred. Please make sure to format the request as follows: Find Case Number 1234");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findcasesubject = criteria => {
-
-    return new Promise((resolve, reject) => {
-         let q = "SELECT Id, Subject, CaseNumber, Estimated_Hours__c, Submitted_By__c, Project_Target_Start_Date__c, Project_Target_Live_Date__c, Description, Resource_Assignment_Due_Date__c, Status,Project_Scope__c, AS_Assignments__c, Account_Manager_Assigned__c, Project_Manager_Assigned__c, SE_assigned_to_GTM_Project__c FROM Case WHERE Subject LIKE '%" + criteria + "%' AND RecordTypeId = '012600000005OU0' ORDER BY Resource_Assignment_Due_Date__c DESC LIMIT 20";
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred. Please make sure to format the request as follows: Find Case Number 1234");
-            } else {
+                reject("An error as occurred. Fix yo code!");
+            } 		
+			else {
                 resolve(resp.records);
             }
         });
@@ -142,9 +167,7 @@ let findcasesubject = criteria => {
 login();
 
 exports.org = org;
-exports.findmyopencases = findmyopencases;
-exports.findmyclosedcases = findmyclosedcases;
-exports.findcasenumber = findcasenumber;
-exports.findcaseowner = findcaseowner;
-exports.findcasesubject = findcasesubject;
-exports.createCase = createCase;
+exports.findshoutouts = findshoutouts;
+exports.findshoutoutweek = findshoutoutweek;
+exports.createShoutout = createShoutout;
+exports.createVote = createVote;
