ce382d346e8651aa62043354da5bd8f63c119b13
ckim559 <ckim@freewheel.tv>
2017-03-23 17:09:36 -0400

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -121,6 +121,60 @@ let createVote = newVote => {
 		
         org.insert({sobject: c}, err => {
             if (err) {
+org = nforce.createConnection({
+		environment: 'sandbox',
+        clientId: SF_CLIENT_ID,
+        clientSecret: SF_CLIENT_SECRET,
+        redirectUri: 'http://localhost:3000/oauth/_callback',
+        mode: 'single',
+        autoRefresh: true
+    });
+
+let login = () => {
+
+    org.authenticate({username: SF_USER_NAME, password: SF_PASSWORD}, err => {
+        if (err) {
+            console.error("Authentication error");
+            console.error(err);
+        } else {
+            console.log("Authentication successful");
+        }
+    });
+
+};
+
+let createShoutout = newShoutout => {
+
+    return new Promise((resolve, reject) => {
+		
+        let c = nforce.createSObject('Services_Shoutout__c');
+        c.set('Recipient_Name__c', newShoutout.person);
+		c.set('Reason__c', newShoutout.reason);
+		c.set('Shoutout_Week__c', newShoutout.id);
+		c.set('ID__c', newShoutout.slackId);
+		
+        org.insert({sobject: c}, err => {
+            if (err) {
+                console.error(err);
+                reject("An error occurred while creating a Shoutout");
+            } else {
+                resolve(c);
+            }
+        });
+    });
+
+};
+
+let createVote = newVote => {
+
+    return new Promise((resolve, reject) => {
+		
+        let c = nforce.createSObject('Shoutout_Vote__c');
+		c.set('Services_Shoutout__c', newVote.id);
+		c.set('Slack_ID__c', newVote.slackId);
+		
+        org.insert({sobject: c}, err => {
+            if (err) {
                 console.error(err);
                 reject("An error occurred while casting your vote.");
             } else {
@@ -139,21 +193,6 @@ let findshoutouts = shouts => {
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred. Fix yo code!");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findshoutoutweek = week => {
-
-    return new Promise((resolve, reject) => {
-         let q = "SELECT ID, Name FROM Shoutout_Week__c WHERE Open_Voting__c = TRUE LIMIT 1";
-         org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred. Fix yo code!");
             } 		
 			else {
                 resolve(resp.records);
@@ -168,6 +207,5 @@ login();
 
 exports.org = org;
 exports.findshoutouts = findshoutouts;
-exports.findshoutoutweek = findshoutoutweek;
 exports.createShoutout = createShoutout;
 exports.createVote = createVote;
